b9ee2b323521f4191eb533d04ab02b2da8cb3b5b
nh325 <104600815+nh325@users.noreply.github.com>
2023-06-08 21:05:35 -0400

    Add files via upload

---
+++ loaddata.m
@@ -0,0 +1,16 @@
+% Load the analysis results
+load(fullfile(folderName, 'analysis_results.mat'));
+
+% Access the width, height, and area attributes for all ROIs within each frame and all the frames
+for m = 1:numel(results)
+    frameResults = results(m);
+    roiWidths = frameResults.Width;
+    roiHeights = frameResults.Height;
+    roiAreas = frameResults.Area;
+    
+    fprintf('Frame %d:\n', m);
+    for n = 1:numel(roiWidths)
+        fprintf('ROI %d: Width = %.2f, Height = %.2f, Area = %.2f\n', n, roiWidths{n}, roiHeights{n}, roiAreas{n});
+    end
+    fprintf('\n');
+end
