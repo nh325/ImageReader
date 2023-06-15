f88889e1d121a410e587aa7af68eef607e864b83
nh325 <104600815+nh325@users.noreply.github.com>
2023-06-14 20:23:41 -0400

    Update loaddata.m

--- loaddata.m
+++ loaddata.m
@@ -7,10 +7,11 @@ for m = 1:numel(results)
     roiWidths = frameResults.Width;
     roiHeights = frameResults.Height;
     roiAreas = frameResults.Area;
+    roiPseudo = frameResults.PseudoFWHM;
     
     fprintf('Frame %d:\n', m);
     for n = 1:numel(roiWidths)
-        fprintf('ROI %d: Width = %.2f, Height = %.2f, Area = %.2f\n', n, roiWidths{n}, roiHeights{n}, roiAreas{n});
+        fprintf('ROI %d: Width = %.2f, Height = %.2f, Area = %.2f\n, PseudoFWHM = %.2f\n', n, roiWidths{n}, roiHeights{n}, roiAreas{n}, roiPseudo{n});
     end
     fprintf('\n');
 end
