12cf032da7c6f4bbdc977e7fe1b8bf7726ffd84c
nh325 <104600815+nh325@users.noreply.github.com>
2023-06-14 21:47:11 -0400

    Update compare_data.m

--- compare_data.m
+++ compare_data.m
@@ -1,10 +1,12 @@
-% Load the analysis results from the first mat file
+% File paths
 folderName1 = 'radon_wk1';
+folderName2 = 'radon_wk8';
+
+% Load the analysis results from the first mat file
 load(fullfile(folderName1, 'analysis_results.mat'));
 results1 = results;
 
 % Load the analysis results from the second mat file
-folderName2 = 'radon_wk8';
 load(fullfile(folderName2, 'analysis_results.mat'));
 results2 = results;
 
@@ -12,15 +14,15 @@ results2 = results;
 numResults1 = numel(results1);
 numResults2 = numel(results2);
 
-widthData1 = NaN(numResults1, 1);
-heightData1 = NaN(numResults1, 1);
-areaData1 = NaN(numResults1, 1);
-pseudoFWHMData1 = NaN(numResults1, 1);
+widthData1 = [];
+heightData1 = [];
+areaData1 = [];
+pseudoFWHMData1 = [];
 
-widthData2 = NaN(numResults2, 1);
-heightData2 = NaN(numResults2, 1);
-areaData2 = NaN(numResults2, 1);
-pseudoFWHMData2 = NaN(numResults2, 1);
+widthData2 = [];
+heightData2 = [];
+areaData2 = [];
+pseudoFWHMData2 = [];
 
 % Extract the widths, heights, areas, and pseudoFWHM data for each week
 for m = 1:numResults1
@@ -29,10 +31,10 @@ for m = 1:numResults1
     roiAreas = results1(m).Area;
     roiPseudo = results1(m).PseudoFWHM;
     
-    widthData1(m) = mean(cell2mat(roiWidths));
-    heightData1(m) = mean(cell2mat(roiHeights));
-    areaData1(m) = mean(cell2mat(roiAreas));
-    pseudoFWHMData1(m) = mean(cell2mat(roiPseudo));
+    widthData1 = [widthData1; cell2mat(roiWidths)];
+    heightData1 = [heightData1; cell2mat(roiHeights)];
+    areaData1 = [areaData1; cell2mat(roiAreas)];
+    pseudoFWHMData1 = [pseudoFWHMData1; cell2mat(roiPseudo)];
 end
 
 for m = 1:numResults2
@@ -41,33 +43,39 @@ for m = 1:numResults2
     roiAreas = results2(m).Area;
     roiPseudo = results2(m).PseudoFWHM;
     
-    widthData2(m) = mean(cell2mat(roiWidths));
-    heightData2(m) = mean(cell2mat(roiHeights));
-    areaData2(m) = mean(cell2mat(roiAreas));
-    pseudoFWHMData2(m) = mean(cell2mat(roiPseudo));
+    widthData2 = [widthData2; cell2mat(roiWidths)];
+    heightData2 = [heightData2; cell2mat(roiHeights)];
+    areaData2 = [areaData2; cell2mat(roiAreas)];
+    pseudoFWHMData2 = [pseudoFWHMData2; cell2mat(roiPseudo)];
 end
 
+% Combine the data from both weeks
+widthData = [widthData1; widthData2];
+heightData = [heightData1; heightData2];
+areaData = [areaData1; areaData2];
+pseudoFWHMData = [pseudoFWHMData1; pseudoFWHMData2];
+
 % Create group labels
-groupLabels = {'Results wk1', 'Results wk8'};
+groupLabels = [repmat({'Results wk1'}, size(widthData1, 1), 1); repmat({'Results wk8'}, size(widthData2, 1), 1)];
 
 % Figure: Box Plots
 figure;
 subplot(2, 2, 1);
-boxplot([widthData1, widthData2], 'Labels', groupLabels);
+boxplot(widthData', groupLabels);
 title('Width Box Plot');
 ylabel('Width');
 
 subplot(2, 2, 2);
-boxplot([heightData1, heightData2], 'Labels', groupLabels);
+boxplot(heightData', groupLabels);
 title('Height Box Plot');
 ylabel('Height');
 
 subplot(2, 2, 3);
-boxplot([areaData1, areaData2], 'Labels', groupLabels);
+boxplot(areaData', groupLabels);
 title('Area Box Plot');
 ylabel('Area');
 
 subplot(2, 2, 4);
-boxplot([pseudoFWHMData1, pseudoFWHMData2], 'Labels', groupLabels);
+boxplot(pseudoFWHMData', groupLabels);
 title('PseudoFWHM Box Plot');
 ylabel('PseudoFWHM');
